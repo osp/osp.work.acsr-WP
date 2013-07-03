66bc941244dabf9e0be3e549e894bcbf62d8ab03
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-07-03 17:10:04 +0200

    lien et rollover sur logo home

--- sidebar.php
+++ sidebar.php
@@ -10,11 +10,13 @@
     <div id="logo" role="logo">
         <h1>
         <?php if (is_home()): ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo_intro.png" alt="l'atelier de création sonore radiophonique" border="0" />
+            <a href="<?php echo home_url(); ?>">
+                <img id="logogris" src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo-web-roll.png" alt="l'atelier de création sonore radiophonique" border="0" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo_intro.png" alt="l'atelier de création sonore radiophonique" border="0" /></a>
         <?php else : ?>
-         <a href="<?php echo home_url(); ?>">
-            <img id="logogris" src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo-web-roll.png" alt="l'atelier de création sonore radiophonique" border="0" />
-            <img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo-web.png" alt="l'atelier de création sonore radiophonique" border="0" />
+            <a href="<?php echo home_url(); ?>">
+                <img id="logogris" src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo-web-roll.png" alt="l'atelier de création sonore radiophonique" border="0" />
+                <img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logos/acsr_logo-web.png" alt="l'atelier de création sonore radiophonique" border="0" />
             </a>
         <?php endif;?>
         </h1>
