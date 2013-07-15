2f175f6afcd97260b7e49d767d9d7e21635d4cc3
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-07-15 15:58:10 +0200

    rm moche navigation en haut de la recherche

--- search.php
+++ search.php
@@ -15,7 +15,6 @@ get_header(); ?>
                 <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'acsr' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
             </header>
 
-            <?php acsr_content_nav( 'nav-above' ); ?>
 
             <?php /* Start the Loop */ ?>
             <?php while ( have_posts() ) : the_post(); ?>
